078e6786747426490eec9825e9715a392d775fc6
pEsquerdo <pedroesquerdo87@gmail.com>
2021-08-11 12:46:21 -0300

    implement timer duration controller - #11

--- app/helpers/match_rooms_helper.rb
+++ app/helpers/match_rooms_helper.rb
@@ -1,2 +1,7 @@
 module MatchRoomsHelper
+  def duration_display(duration_value)
+    duration = ActiveSupport::Duration.build(duration_value).parts
+
+    format('%02d:%02d:%02d', (duration[:hours] || 0), duration[:minutes] || 0, duration[:seconds] || 0)
+  end
 end
